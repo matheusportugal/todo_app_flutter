939aabb9a6a459bf8366ad7b3dee0e6545b3ced6
Matheus Portugal <matheusportugal@yahoo.com.br>
2021-05-12 18:58:05 -0300

    Final App

--- lib/screens/todo_item.dart
+++ lib/screens/todo_item.dart
@@ -5,32 +5,61 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'package:todo_app/models/Todo.dart';
 
 class TodoItem extends StatefulWidget {
-  const TodoItem({Key key}) : super(key: key);
+  final Todo todo;
+  final int index;
+  TodoItem({Key key, @required this.todo, @required this.index}) : super(key: key);
 
   @override
-  _TodoItemState createState() => _TodoItemState();
+  _TodoItemState createState() => _TodoItemState(todo, index);
 }
 
 class _TodoItemState extends State<TodoItem> {
+  Todo _todo;
+  int _index;
+
   final _tituloController = TextEditingController();
   final _descricaoController = TextEditingController();
 
+  final key = GlobalKey<ScaffoldState>();
+
+  _TodoItemState(Todo todo, int index) {
+    this._todo = todo;
+    this._index = index;
+    if (todo != null) {
+      _tituloController.text = _todo.titulo;
+      _descricaoController.text = _todo.descricao;
+    }
+  }
+
   _saveItem() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    List<Todo> list = [];
-    var data = prefs.getString('list');
-    if (data != null) {
+    if (_tituloController.text.isEmpty || _descricaoController.text.isEmpty) {
+      key.currentState.showSnackBar(SnackBar(
+          content: Text('Titulo e descrição são obrigatórios.')
+      ));
+    } else {
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+      List<Todo> list = [];
+      var data = prefs.getString('list');
+      if (data != null) {
         var objs = jsonDecode(data) as List;
         list = objs.map((obj) => Todo.fromJson(obj)).toList();
-    }
-    debugPrint(list.toString());
-    Todo todo = Todo.fromTituloDescricao(_tituloController.text, _descricaoController.text);
+      }
 
+      _todo = Todo.fromTituloDescricao(_tituloController.text, _descricaoController.text);
+      if (_index != -1 ) {
+        list[_index] = _todo;
+      } else {
+        list.add(_todo);
+      }
+      prefs.setString('list', jsonEncode(list));
+      Navigator.pop(context);
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: key,
       appBar: AppBar(
         backgroundColor: Colors.greenAccent, title: Text('Todo Item'),
       ),
--- lib/screens/todo_list.dart
+++ lib/screens/todo_list.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:todo_app/models/Todo.dart';
@@ -12,6 +14,7 @@ class TodoList extends StatefulWidget {
 
 class _TodoListState extends State<TodoList> {
   List<Todo> list = [];
+  final _doneStyle = TextStyle(color: Colors.green, decoration: TextDecoration.lineThrough);
 
   @override
   void initState() {
@@ -22,7 +25,56 @@ class _TodoListState extends State<TodoList> {
   _reloadList() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     var data = prefs.getString(('list'));
-    debugPrint(data);
+    if (data != null) {
+      setState(() {
+        var objs = jsonDecode(data) as List;
+        list = objs.map((obj) => Todo.fromJson(obj)).toList();
+      });
+    }
+  }
+
+  _removeItem(int index) {
+    setState(() {
+      list.removeAt(index);
+    });
+
+    SharedPreferences.getInstance().then((prefs) =>
+        prefs.setString('list', jsonEncode(list)));
+  }
+
+  _doneItem(int index) {
+    setState(() {
+      list[index].status = 'F';
+    });
+
+    SharedPreferences.getInstance().then((prefs) =>
+        prefs.setString('list', jsonEncode(list)));
+
+  }
+  
+  _showAlertDialog(BuildContext context, String conteudo, Function confirmFunction, int index) {
+    showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: Text('Confirmação'),
+            content: Text(conteudo),
+            actions: [
+              FlatButton(
+                  onPressed: () => Navigator.pop(context),
+                  child: Text('Não')
+              ),
+              FlatButton(
+                  onPressed: () {
+                    confirmFunction(index);
+                    Navigator.pop(context);
+                  },
+                  child: Text('Sim')
+              ),
+            ],
+          );
+        }
+    );
   }
 
   @override
@@ -36,12 +88,32 @@ class _TodoListState extends State<TodoList> {
           itemCount: list.length,
           itemBuilder: (context, index) {
               return ListTile(
-                  title: Text('${list[index].titulo}'),
-                  subtitle: Text('${list[index].descricao}'),
+                  title: Text('${list[index].titulo}',
+                      style: list[index].status == 'F' ? _doneStyle : null),
+                  subtitle: Text('${list[index].descricao}',
+                      style: list[index].status == 'F' ? _doneStyle : null),
                   onTap: () => Navigator.push(
                     context,
-                    MaterialPageRoute(builder: (context) => TodoItem(),)
-                  ),
+                    MaterialPageRoute(builder: (context) => TodoItem(todo: list[index], index: index),
+                    )
+                  ).then((value) => _reloadList()),
+                trailing: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    IconButton(
+                      icon:Icon(Icons.clear),
+                      onPressed: () => _showAlertDialog(context, 'Confirma a exclusão deste item?', _removeItem, index),
+                    ),
+                    Visibility(
+                      visible: list[index].status == 'A',
+                      child: IconButton(
+                        icon:Icon(Icons.check),
+                        onPressed: () => _showAlertDialog(context, 'Confirma a finalização deste item?', _doneItem, index),
+                      ),
+                    )
+
+                  ],
+                ),
               );
           },
       ),
@@ -50,8 +122,9 @@ class _TodoListState extends State<TodoList> {
         child: Icon(Icons.add),
         onPressed: () => Navigator.push(
             context,
-            MaterialPageRoute(builder: (context) => TodoItem(),)
-        ),
+            MaterialPageRoute(builder: (context) => TodoItem(todo: null, index: -1),
+            )
+        ).then((value) => _reloadList()),
       ),
     );
   }
